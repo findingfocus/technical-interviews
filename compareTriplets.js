562b204697d9498af456e20a50e8484f9409ebd3
Paul Thompson <paul@findingfocus.dev>
2024-10-17 22:14:25 -0600

    add compare triplets function

---
+++ compareTriplets.js
@@ -0,0 +1,12 @@
+function compareTriplets(a, b) {
+   let points = [0,0];
+   for (let i = 0; i < a.length; i++) {
+       if (a[i] > b[i]) {
+           points[0]++;
+       }
+       if (b[i] > a[i]) {
+           points[1]++;
+       }
+   }
+   return points;
+}
